d865f32a0adb1eb4a8a26535767529325a394ec6
Ricky Marasigan <rickysoliman@gmail.com>
2023-01-31 15:07:59 -0500

    using mat-toolbar in header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { ContentComponent } from './components/content/content.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.model.ts
+++ src/app/components/header/header.model.ts
@@ -5,7 +5,7 @@ type IconLink = {
 
 export const iconLinks: IconLink[] = [
     { icon: 'Github', url: 'https://github.com/rickysoliman' },
-    { icon: 'LinkedIn', url: 'https://media.licdn.com/dms/image/D5635AQFar6QFJdqOgQ/profile-framedphoto-shrink_400_400/0/1672068522776?e=1675796400&v=beta&t=I3XCLlXGD4UULwa5uUwGeZ0BQJJHwTtRpU1WzumhyNM' },
+    { icon: 'LinkedIn', url: 'https://linkedin.com/in/ricky-marasigan' },
 ];
 
 export const logoUrl = 'https://media.licdn.com/dms/image/C5603AQGo4ZD1sfeSzA/profile-displayphoto-shrink_100_100/0/1610752741704?e=1680739200&v=beta&t=wEv_sLJDMfoGG9PksTfI0raexNE4ejrpHjmvEqP9Hlw';
