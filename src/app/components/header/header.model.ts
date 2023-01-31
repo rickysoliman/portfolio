24c08c2f691517f551d1899ee6d33dca0db85aa2
Ricky Marasigan <rickysoliman@gmail.com>
2023-01-31 11:47:13 -0500

    progress on header and some basic styling

---
+++ src/app/components/header/header.model.ts
@@ -0,0 +1,11 @@
+type IconLink = {
+    url: string,
+    icon: string,
+}
+
+export const logoUrl = './headshot.JPG';
+
+export const iconLinks: IconLink[] = [
+    { icon: 'Github', url: 'github.com/rickysoliman' },
+    { icon: 'LinkedIn', url: 'linkedin.com/in/ricky-marasigan/' },
+];
