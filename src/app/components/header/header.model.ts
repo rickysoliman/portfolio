26be75d122cad6f0774b1d8fad7bcc12c9edf2f5
Ricky Marasigan <rickysoliman@gmail.com>
2023-01-31 15:27:45 -0500

    social media icons

--- src/app/components/header/header.model.ts
+++ src/app/components/header/header.model.ts
@@ -1,11 +1,12 @@
 type IconLink = {
     url: string,
-    icon: string,
+    name: string,
+    class: string,
 }
 
 export const iconLinks: IconLink[] = [
-    { icon: 'Github', url: 'https://github.com/rickysoliman' },
-    { icon: 'LinkedIn', url: 'https://linkedin.com/in/ricky-marasigan' },
+    { name: 'Github', url: 'https://github.com/rickysoliman', class: 'fab fa-github' },
+    { name: 'LinkedIn', url: 'https://linkedin.com/in/ricky-marasigan', class: 'fab fa-linkedin' },
 ];
 
 export const logoUrl = 'https://media.licdn.com/dms/image/C5603AQGo4ZD1sfeSzA/profile-displayphoto-shrink_100_100/0/1610752741704?e=1680739200&v=beta&t=wEv_sLJDMfoGG9PksTfI0raexNE4ejrpHjmvEqP9Hlw';
