ef691eff05656f82f6eb8fdd4e46b5a73671acfd
Ricky Marasigan <rickysoliman@gmail.com>
2023-06-28 10:42:56 -0400

    update gif and description for dall-e clone

--- src/app/components/content/projects.model.ts
+++ src/app/components/content/projects.model.ts
@@ -8,14 +8,14 @@ type Project = {
 export const projects: Project[] = [
   {
     name: 'Word-Chain',
-    description: 'An engaging online word game developed in Angular. It leverages the Free Dictionary API to validate words and challenges players to construct a chain of words within a 60-second timeframe. Test your vocabulary and spelling abilities by entering a word that starts with the last letter of the previous word in the chain and strive to build the longest chain possible!',
+    description: `An engaging online word game developed in Angular. It leverages the Free Dictionary API to validate words and challenges players to construct a chain of words within a 60-second timeframe. Test your vocabulary and spelling abilities by entering a word that starts with the last letter of the previous word in the chain and strive to build the longest chain possible!`,
     url: 'https://rickysoliman.github.io/word-chain/',
     gifPath: '../../../assets/word-chain.gif',
   },
   {
     name: 'Dall-E Clone',
-    description: `This is a simple re-creation of the UI for OpenAI's DALL·E Image Generation app.`,
+    description: `This is my own take on the UI of the official DALL-E website. Built in React, this app enables users to input text prompts and call upon the DALL-E Image Generation API to generate and display one or multiple images based on the given prompt.`,
     url: 'https://rickysoliman.github.io/simple-dall-e-clone/',
-    gifPath: '',
+    gifPath: '../../../assets/dalle.gif',
   }
 ];
