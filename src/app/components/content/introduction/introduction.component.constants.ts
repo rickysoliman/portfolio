d2029de3c3f843f829b1915aa54972cfe3fb3fba
Ricky Marasigan <rickysoliman@gmail.com>
2023-06-28 10:26:41 -0400

    fixed background image

--- src/app/components/content/introduction/introduction.component.constants.ts
+++ src/app/components/content/introduction/introduction.component.constants.ts
@@ -1,4 +1,4 @@
 export const introText = {
     header: `Hi, I'm Ricky!`,
-    paragraph: `I'm a web developer based in Washington, DC and I love to build things for the web. Check out some things I've built and feel free to reach out!`,
+    paragraph: `I'm a software engineer based in Washington, DC and I love to build things for the web. Check out some things I've built and feel free to reach out!`,
 };
