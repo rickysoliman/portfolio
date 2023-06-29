f7e357a58477d204a7a3a01836011b2dae05beda
Ricky Marasigan <rickysoliman@gmail.com>
2023-06-29 10:20:37 -0400

    fad in animations on scroll

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,17 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { projects, workExperience } from './experience.model';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      state('void', style({ opacity: 0 })),
+      state('*', style({ opacity: 1 })),
+      transition(':enter, :leave', animate('0.5s ease')),
+    ]),
+  ],
 })
-export class ContentComponent implements OnInit {
+export class ContentComponent implements AfterViewInit {
   projects = projects;
   workExperience = workExperience;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) {}
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    const elements = this.elementRef.nativeElement.querySelectorAll('.fade-in');
+
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1, // Adjust this value to control when the fading occurs
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, options);
+
+    elements.forEach((element: any) => {
+      observer.observe(element);
+    });
   }
 
 }
