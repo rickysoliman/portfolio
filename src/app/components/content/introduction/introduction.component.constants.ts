08a38db672f408aabc43ab9c5b808d2da970f68e
Ricky Marasigan <rickysoliman@gmail.com>
2023-06-29 13:01:51 -0400

    background gradient, emoji, conclusion, more

--- src/app/components/content/introduction/introduction.component.constants.ts
+++ src/app/components/content/introduction/introduction.component.constants.ts
@@ -1,4 +1,4 @@
 export const introText = {
-    header: `Hi, I'm Ricky!`,
+    header: `Hi!`,
     paragraph: `I'm a software engineer based in Washington, DC and I love to build things for the web. Check out some things I've built and feel free to reach out!`,
 };
