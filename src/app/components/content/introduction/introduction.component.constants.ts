5d0a1eea8906297b0679467efad0bc6bc289cc66
Ricky Marasigan <rickysoliman@gmail.com>
2023-07-19 13:32:41 -0400

    add icons to technical skills list

--- src/app/components/content/introduction/introduction.component.constants.ts
+++ src/app/components/content/introduction/introduction.component.constants.ts
@@ -3,19 +3,24 @@ export const introText = {
     paragraph: `I'm a software engineer based in Washington, DC and I love to build things for the web. Check out some things I've built and feel free to reach out!`,
 };
 
-export const technicalSkills = [
-  'JavaScript',
-  'TypeScript',
-  'Python',
-  'HTML',
-  'CSS',
-  'Angular',
-  'RxJS',
-  'GraphQL',
-  'REST API',
-  'React',
-  'Jest',
-  'Cypress',
-  'Node.js',
-  'Git',
+type TechnicalSkill = {
+  name: string;
+  iconPath: string;
+};
+
+export const technicalSkills: TechnicalSkill[] = [
+  { name: 'JavaScript', iconPath: '../../../../assets/skill-icons/javascript.png', },
+  { name: 'TypeScript', iconPath: '../../../../assets/skill-icons/typescript.png', },
+  { name: 'Python', iconPath: '../../../../assets/skill-icons/python.png', },
+  { name: 'HTML', iconPath: '../../../../assets/skill-icons/html.png', },
+  { name: 'CSS', iconPath: '../../../../assets/skill-icons/css.png', },
+  { name: 'Angular', iconPath: '../../../../assets/skill-icons/angular.png', },
+  { name: 'RxJS', iconPath: '../../../../assets/skill-icons/rxjs.png', },
+  { name: 'GraphQL', iconPath: '../../../../assets/skill-icons/graphql.png', },
+  { name: 'RESTAPI' , iconPath: '../../../../assets/skill-icons/api.png', },
+  { name: 'React', iconPath: '../../../../assets/skill-icons/react.png', },
+  { name: 'Jest', iconPath: '../../../../assets/skill-icons/jest.png', },
+  { name: 'Cypress', iconPath: '../../../../assets/skill-icons/cypress.png', },
+  { name: 'Node.js', iconPath: '../../../../assets/skill-icons/node.png', },
+  { name: 'Git', iconPath: '../../../../assets/skill-icons/git.png', },
 ];
