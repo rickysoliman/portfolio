f6a84f3de45013a215a2ba96ed3fcaabe2bba561
Ricky Marasigan <rickysoliman@gmail.com>
2025-08-01 14:03:06 -0400

    add content to work experience

--- src/app/components/content/experience.constants.ts
+++ src/app/components/content/experience.constants.ts
@@ -1,16 +1,16 @@
 type Project = {
-  name: string,
-  description: string,
-  url: string,
-  gifPath: string,
+  name: string;
+  description: string;
+  url: string;
+  gifPath: string;
 };
 
 type WorkExperience = {
-  company: string,
-  title: string,
-  date: string,
-  description: string,
-  logoPath: string,
+  company: string;
+  title: string;
+  date: string;
+  description: string;
+  logoPath: string;
 };
 
 export const projects: Project[] = [
@@ -25,27 +25,36 @@ export const projects: Project[] = [
     description: `A re-imagining of OpenAI's DALL·E Image Generation app. Just describe anything you can think of in your wildest dreams, and click Generate. This React based app will utilize OpenAI's Image Generation API to bring your imagination to life!`,
     url: '',
     gifPath: '../../../assets/dalle.gif',
-  }
+  },
 ];
 
 export const workExperience: WorkExperience[] = [
   {
-    company: 'Bill.com',
-    title: 'Software Engineer II - Frontend UI',
-    date: 'July 2021 - November 2022',
+    company: 'Verra, Inc.',
+    title: 'Lead Frontend Software Engineer',
+    date: 'October 2023 – June 2024',
     description: `
-      At Bill.com, I had the pleasure of working on bug fixes and new features with an amazing team of engineers who I learned so much from. Along with developing features and writing unit and end to end tests, I also got to integrate the Google Places API into our address forms, design a set of dynamic components that can be utilized throughout the application, and lots more!
+      At Verra, I served as the Lead Frontend Software Engineer on a small, fast-paced team. I spearheaded the development of scalable and accessible UI components using Angular 15, TypeScript, and SCSS, and led a full UI refactor that aligned the platform with modern design standards and WCAG accessibility guidelines. I collaborated closely with product managers, designers, and backend developers in an Agile environment to deliver high-impact features for our carbon credit registry system. I also played a key role in mentoring junior developers and conducting code reviews to ensure high-quality, maintainable code.
     `,
-    logoPath: '../../../assets/bill-logo.png',
+    logoPath: '../../../assets/verra-logo.png',
   },
   {
     company: 'Disaster Accountability Project',
     title: 'Volunteer Frontend Software Engineer',
-    date: 'May 2023 - Present',
+    date: 'January 2023 – September 2023',
     description: `
       I’ve been enjoying doing volunteer work for the Disaster Accountability Project very much by contributing to the revamping of their website. I've built several Angular components, including a re-usable and dynamic text input component, and a slick hamburger icon drop-down menu. With a keen eye for quality, I crafted solid unit tests using Jest and conducted thorough end-to-end tests using Cypress. Throughout the process, I also actively participated in code reviews, offering constructive feedback and fostering a friendly and collaborative team environment.
     `,
     logoPath: '../../../assets/dap-logo.jpg',
+  },
+  {
+    company: 'Bill.com',
+    title: 'Software Engineer II - Frontend UI',
+    date: 'July 2021 - December 2022',
+    description: `
+      At Bill.com, I had the pleasure of working on bug fixes and new features with an amazing team of engineers who I learned so much from. Along with developing features and writing unit and end to end tests, I also got to integrate the Google Places API into our address forms, design a set of dynamic components that can be utilized throughout the application, and lots more!
+    `,
+    logoPath: '../../../assets/bill-logo.png',
   },
   {
     company: 'All for the Family Legal Clinic, Inc.',
--- src/app/components/content/introduction/introduction.component.constants.ts
+++ src/app/components/content/introduction/introduction.component.constants.ts
@@ -1,6 +1,6 @@
 export const introText = {
-    header: `Hi!`,
-    paragraph: `I'm a software engineer based in Washington, DC and I love to build things for the web. Check out some things I've built and feel free to reach out!`,
+  header: `Hi!`,
+  paragraph: `I'm a software engineer based in the Washington DMV Metro area and I love to build things for the web. Check out some things I've built and feel free to reach out!`,
 };
 
 type TechnicalSkill = {
@@ -9,18 +9,24 @@ type TechnicalSkill = {
 };
 
 export const technicalSkills: TechnicalSkill[] = [
-  { name: 'JavaScript', iconPath: '../../../../assets/skill-icons/javascript.png', },
-  { name: 'TypeScript', iconPath: '../../../../assets/skill-icons/typescript.png', },
-  { name: 'Python', iconPath: '../../../../assets/skill-icons/python.png', },
-  { name: 'HTML', iconPath: '../../../../assets/skill-icons/html.png', },
-  { name: 'CSS', iconPath: '../../../../assets/skill-icons/css.png', },
-  { name: 'Angular', iconPath: '../../../../assets/skill-icons/angular.png', },
-  { name: 'RxJS', iconPath: '../../../../assets/skill-icons/rxjs.png', },
-  { name: 'GraphQL', iconPath: '../../../../assets/skill-icons/graphql.png', },
-  { name: 'RESTAPI' , iconPath: '../../../../assets/skill-icons/api.png', },
-  { name: 'React', iconPath: '../../../../assets/skill-icons/react.png', },
-  { name: 'Jest', iconPath: '../../../../assets/skill-icons/jest.png', },
-  { name: 'Cypress', iconPath: '../../../../assets/skill-icons/cypress.png', },
-  { name: 'Node.js', iconPath: '../../../../assets/skill-icons/node.png', },
-  { name: 'Git', iconPath: '../../../../assets/skill-icons/git.png', },
+  {
+    name: 'JavaScript',
+    iconPath: '../../../../assets/skill-icons/javascript.png',
+  },
+  {
+    name: 'TypeScript',
+    iconPath: '../../../../assets/skill-icons/typescript.png',
+  },
+  { name: 'Python', iconPath: '../../../../assets/skill-icons/python.png' },
+  { name: 'HTML', iconPath: '../../../../assets/skill-icons/html.png' },
+  { name: 'CSS', iconPath: '../../../../assets/skill-icons/css.png' },
+  { name: 'Angular', iconPath: '../../../../assets/skill-icons/angular.png' },
+  { name: 'RxJS', iconPath: '../../../../assets/skill-icons/rxjs.png' },
+  { name: 'GraphQL', iconPath: '../../../../assets/skill-icons/graphql.png' },
+  { name: 'RESTAPI', iconPath: '../../../../assets/skill-icons/api.png' },
+  { name: 'React', iconPath: '../../../../assets/skill-icons/react.png' },
+  { name: 'Jest', iconPath: '../../../../assets/skill-icons/jest.png' },
+  { name: 'Cypress', iconPath: '../../../../assets/skill-icons/cypress.png' },
+  { name: 'Node.js', iconPath: '../../../../assets/skill-icons/node.png' },
+  { name: 'Git', iconPath: '../../../../assets/skill-icons/git.png' },
 ];
