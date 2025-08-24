fc29472fb69378674bbe9ed99dbd5f9d393e0e9d
Ricky Marasigan <rickysoliman@gmail.com>
2025-08-24 02:24:32 -0400

    technical skills style changes

--- src/app/components/content/introduction/introduction.component.constants.ts
+++ src/app/components/content/introduction/introduction.component.constants.ts
@@ -9,24 +9,18 @@ type TechnicalSkill = {
 };
 
 export const technicalSkills: TechnicalSkill[] = [
-  {
-    name: 'JavaScript',
-    iconPath: 'assets/skill-icons/javascript.png',
-  },
-  {
-    name: 'TypeScript',
-    iconPath: 'assets/skill-icons/typescript.png',
-  },
-  { name: 'Python', iconPath: 'assets/skill-icons/python.png' },
+  { name: 'JavaScript', iconPath: 'assets/skill-icons/javascript.png' },
+  { name: 'TypeScript', iconPath: 'assets/skill-icons/typescript.png' },
   { name: 'HTML', iconPath: 'assets/skill-icons/html.png' },
-  { name: 'CSS', iconPath: 'assets/skill-icons/css.png' },
   { name: 'Angular', iconPath: 'assets/skill-icons/angular.png' },
   { name: 'RxJS', iconPath: 'assets/skill-icons/rxjs.png' },
+  { name: 'React', iconPath: 'assets/skill-icons/react.png' },
   { name: 'GraphQL', iconPath: 'assets/skill-icons/graphql.png' },
   { name: 'RESTAPI', iconPath: 'assets/skill-icons/api.png' },
-  { name: 'React', iconPath: 'assets/skill-icons/react.png' },
   { name: 'Jest', iconPath: 'assets/skill-icons/jest.png' },
   { name: 'Cypress', iconPath: 'assets/skill-icons/cypress.png' },
   { name: 'Node.js', iconPath: 'assets/skill-icons/node.png' },
   { name: 'Git', iconPath: 'assets/skill-icons/git.png' },
+  { name: 'Postman', iconPath: 'assets/skill-icons/postman.png' },
+  { name: 'Jira', iconPath: 'assets/skill-icons/jira.png' },
 ];
