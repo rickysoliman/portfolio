961cba35be70c5e4cbc3b60eba9ebf01627c753a
Ricky Marasigan <rickysoliman@gmail.com>
2025-08-27 03:11:04 -0400

    mouse spotlight & background color change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    const spotlight = document.getElementById('mouse-spotlight');
+    if (spotlight) {
+      const offsetX = spotlight.offsetWidth / 2;
+      const offsetY = spotlight.offsetHeight / 2;
+      spotlight.style.transform = `translate(${e.clientX - offsetX}px, ${
+        e.clientY - offsetY
+      }px)`;
+    }
+  }
 }
