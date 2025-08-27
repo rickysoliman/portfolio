f2bf20eabc51913141deebb2f6c6400885a1dbcb
Ricky Marasigan <rickysoliman@gmail.com>
2025-08-27 11:26:29 -0400

    mouse spotlight disappears when mouse leaves window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,28 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: MouseEvent) {
     const spotlight = document.getElementById('mouse-spotlight');
     if (spotlight) {
-      const offsetX = spotlight.offsetWidth / 2;
-      const offsetY = spotlight.offsetHeight / 2;
-      spotlight.style.transform = `translate(${e.clientX - offsetX}px, ${
-        e.clientY - offsetY
-      }px)`;
+      spotlight.style.transform = `translate(${e.clientX}px, ${e.clientY}px) translate(-50%, -50%)`;
+    }
+  }
+
+  @HostListener('document:mouseleave')
+  onMouseLeave() {
+    const spotlight = document.getElementById('mouse-spotlight');
+    if (spotlight) {
+      spotlight.style.opacity = '0';
+    }
+  }
+
+  @HostListener('document:mouseenter')
+  onMouseEnter() {
+    const spotlight = document.getElementById('mouse-spotlight');
+    if (spotlight) {
+      spotlight.style.opacity = '1';
     }
   }
 }
